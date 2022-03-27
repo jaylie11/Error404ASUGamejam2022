5623a1c5bd4b6c280070b46c7eb19de51276a80f
IgnacioFlores1500 <ignacioflores1500@gmail.com>
2022-03-27 18:07:47 -0500

    Set Debug level with colliders

---
+++ TBD/Assets/Scripts/BrackeysMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrackeysMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+	public float playerSpeed = 1.0f;
+	
+	private float horizontalMove = 0f;
+	bool jump = false;
+	
+	private void update() {
+		horizontalMove = Input.GetAxisRaw("Horizontal") * playerSpeed;
+		
+		if (Input.GetButtonDown("Jump")) {
+			jump = true;
+		}
+	}
+	
+	private void FixedUpdate() {
+		controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+		jump = false;
+	}
+}
--- TBD/Assets/Scripts/PlayerMovement.cs
+++ TBD/Assets/Scripts/PlayerMovement.cs
@@ -6,8 +6,6 @@ public class PlayerMovement : MonoBehaviour
 {
     // Changes PlayerSpeed
 	public float playerSpeed = 1.0f;
-	//Changes Jumpforce of the player.
-	//Note I had the gravity on the player at 3
 	public float jumpForce = 10.0f;
 	/*
 	BoxCollider2D is for the collider
@@ -15,54 +13,74 @@ public class PlayerMovement : MonoBehaviour
 	Vector3 is to create the vector to store the position of the player
 	inair is a check the player so they can't double jump. NOTE: this is actually bugged
 	*/
+	
+	[SerializeField] private LayerMask foreGroundLayerMask;
 	private BoxCollider2D BC2D;
-	private Rigidbody2D rb2D;
+	private Rigidbody2D RB2D;
 	private Vector3 movePosition;
-	private bool inAir = false;
+	//private bool inAir = false;
+	private Vector3 moveDelta;
 	
 	//Grab the gameComponents
-    private void Start()
+    private void Awake()
     {
         BC2D = GetComponent<BoxCollider2D>();
-		rb2D = GetComponent<Rigidbody2D>();
+		RB2D = GetComponent<Rigidbody2D>();
     }
+//####################################################################################################################################################################################
 
-    private void FixedUpdate() { 
+    private void Update() {
 		
+		//Checks to see if the rigidbody is grounded, and if the spacebar is pressed
+		if (isGrounded() && Input.GetKeyDown(KeyCode.Space)) {
+			RB2D.velocity = Vector3.up * jumpForce;
+		}
+
+			
+	}
+	private void FixedUpdate() {
 		//Grab the number between -1 and 1 for Left and right movement
 		float x = Input.GetAxisRaw("Horizontal");
 		
-		//Grabs the current velocity of the player. It's used later for checking the isAir bool
-		Vector3 testLog = rb2D.velocity;
-		//float y = Input.GetAxisRaw("Vertical");
-		
-		if (Input.GetButtonDown("Jump")) {
-			Debug.Log("jumped");
-			//If the player is not in the air, allow them to jump
-			if (!inAir)
-				rb2D.AddForce(transform.up * (32 * jumpForce));
-			inAir = true;
-		}
-		
-		//Checks if the player lands on the floor
-		if (testLog.y == 0.0f) {
-			inAir = false;
-			Debug.Log("airClear");
-		}
-		
-		//If the player is moving left, make the sprite invert to match it
-		if (x < 0) {
+	//###################################################################################################################################################################################################################################################################################
+		//If the last given movement was negative, it will turn the sprite left, if not, it will return it normal.
+		//Note, code might change here depending on the size of the sprites
+		moveDelta = new Vector3(x,0,0);
+		if (moveDelta.x < 0) {
 			transform.localScale = new Vector3(-.16f,.16f,1f);
-		} else {
+		} else if (moveDelta.x > 0) {
 			transform.localScale = new Vector3(.16f,.16f,1f);
 		}
-		
+	//####################################################################################################################################################################################		
 		//Note, we are not changing the Y value since that's for jumping
 		movePosition = new Vector3(x,0,0);
-		
 		transform.Translate(movePosition * Time.fixedDeltaTime * playerSpeed );
+	}
 
-		
-		
-    }
+//####################################################################################################################################################################################
+		//Cast a box from the middle of the player's boxCollider with the size of the actual boxCollider
+		//0f means the box wont be rotated
+		//Store it inside a RaycastHit2D called "raycastHit2d"
+	private bool isGrounded() {
+		RaycastHit2D raycastHit2d = Physics2D.BoxCast(BC2D.bounds.center, BC2D.bounds.size, 0f, Vector2.down, .1f,foreGroundLayerMask);
+		Debug.Log(raycastHit2d.collider);
+		return raycastHit2d.collider != null;
+	}
 }
+//####################################################################################################################################################################################
+//Random Old Code
+	//Grabs the current velocity of the player. It's used later for checking the isAir bool
+	//Vector3 testLog = rb2D.velocity;
+	// if (Input.GetButtonDown("Jump")) {
+		// Debug.Log("jumped");
+		// //If the player is not in the air, allow them to jump
+		// if (!inAir) {
+			// rb2D.AddForce(transform.up * (32 * jumpForce));
+			// inAir = true;
+		// }
+	// }
+	
+	// //Checks if the player lands on the floor
+	// if (testLog.y == 0.0f) {
+		// inAir = false;
+		// Debug.Log("airClear");
